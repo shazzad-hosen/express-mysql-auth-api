c4a91241bb7fc3ad7aa6210a0311f9d8fda35491
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-03-13 15:27:33 +0600

    add error middleware and set it up and add parse time utility

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
+import ApiError from "./utils/ApiError.js";
 import cookieParser from "cookie-parser";
+import errorHandler from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -8,8 +10,14 @@ app.use(cookieParser());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/health", (req, res) => {
-  res.json({ status: "OK" });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API is running successfully" });
 });
+
+app.use((req, res, next) => {
+  next(new ApiError(404, "Route not found"));
+});
+
+app.use(errorHandler);
 
 export default app;
