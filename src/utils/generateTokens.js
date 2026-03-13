073ec4e2744ff674c45421e8fdd935f2e265a1aa
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-03-13 21:05:03 +0600

    add async handler and update token generation logic

--- src/utils/generateTokens.js
+++ src/utils/generateTokens.js
@@ -3,15 +3,29 @@ import crypto from "crypto";
 import { ENV } from "../config/env.js";
 
 export const generateAccessToken = async (user) => {
-  return jwt.sign({ id: user.id }, ENV.JWT_ACCESS_SECRET, {
-    expiresIn: ENV.JWT_ACCESS_EXPIRY,
-  });
+  return jwt.sign(
+    {
+      sub: user.id,
+      role: user.role || "user",
+    },
+    ENV.JWT_ACCESS_SECRET,
+    {
+      expiresIn: ENV.JWT_ACCESS_EXPIRY,
+    },
+  );
 };
 
 export const generateRefreshToken = async (user) => {
-  return jwt.sign({ id: user.id }, ENV.JWT_REFRESH_SECRET, {
-    expiresIn: ENV.JWT_REFRESH_EXPIRY,
-  });
+  return jwt.sign(
+    {
+      id: user.id,
+      jti: crypto.randomBytes(16).toString("hex"),
+    },
+    ENV.JWT_REFRESH_SECRET,
+    {
+      expiresIn: ENV.JWT_REFRESH_EXPIRY,
+    },
+  );
 };
 
 export const hashToken = async (token) => {
