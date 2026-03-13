f9d82f88d4a6d7e40943f5609137d31cce5d60f0
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-03-13 13:03:35 +0600

    add token repository layer

---
+++ src/repositories/token.repository.js
@@ -0,0 +1,23 @@
+import pool from "../config/db.js";
+
+export const createRefreshToken = async ({ userId, tokenHash, expiresAt }) => {
+  const query = ` INSERT INTO refresh_tokens (user_id, token_hash, expires_at)VALUES (?, ?, ?) `;
+
+  const [result] = await pool.execute(query, [userId, tokenHash, expiresAt]);
+
+  return result.insertId;
+};
+
+export const findRefreshToken = async (tokenHash) => {
+  const query = ` SELECT * FROM refresh_tokens WHERE token_hash = ? LIMIT 1 `;
+
+  const [rows] = await pool.execute(query, [tokenHash]);
+
+  return rows[0];
+};
+
+export const deleteRefreshToken = async (id) => {
+  const query = ` DELETE FROM refresh_tokens WHERE id = ? `;
+
+  await pool.execute(query, [id]);
+};
