c2d99685fe0fcc02c16ace6e2f4369cfed4a446c
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-04-24 17:44:11 +0600

    update rate limiter middleware

--- src/middlewares/rateLimiter.middleware.js
+++ src/middlewares/rateLimiter.middleware.js
@@ -1,8 +1,14 @@
-import rateLimit from "express-rate-limit";
+import rateLimit, { ipKeyGenerator } from "express-rate-limit";
 import RedisStore from "rate-limit-redis";
 import { redis } from "../config/redis.js";
 
-const createLimiter = ({ windowMs, max, message }) => {
+const createStore = (prefix) =>
+  new RedisStore({
+    sendCommand: (...args) => redis.call(...args),
+    prefix,
+  });
+
+const createLimiter = ({ windowMs, max, message, prefix }) => {
   return rateLimit({
     windowMs,
     max,
@@ -13,9 +19,9 @@ const createLimiter = ({ windowMs, max, message }) => {
     standardHeaders: true,
     legacyHeaders: false,
 
-    store: new RedisStore({
-      sendCommand: (...args) => redis.call(...args),
-    }),
+    keyGenerator: (req) => req.user?.id || ipKeyGenerator(req),
+
+    store: createStore(prefix),
   });
 };
 
@@ -23,22 +29,34 @@ export const globalLimiter = createLimiter({
   windowMs: 15 * 60 * 1000,
   max: 100,
   message: "Too many requests, please try again later",
+  prefix: "rl:global:",
 });
 
-export const authLimiter = createLimiter({
+export const authLimiter = rateLimit({
   windowMs: 25 * 60 * 1000,
-  max: 10,
-  message: "Too many auth attempts, try again later",
+  max: 15,
+  skipSuccessfulRequests: true,
+
+  message: {
+    success: false,
+    message: "Too many auth attempts, try again later",
+  },
+
+  keyGenerator: (req) => req.user?.id || ipKeyGenerator(req),
+
+  store: createStore("rl:auth:"),
 });
 
 export const otpLimiter = createLimiter({
   windowMs: 15 * 60 * 1000,
-  max: 4,
+  max: 5,
   message: "Too many OTP requests, please wait",
+  prefix: "rl:otp:",
 });
 
 export const refreshLimiter = createLimiter({
   windowMs: 15 * 60 * 1000,
-  max: 10,
+  max: 12,
   message: "Too many refresh attempts",
+  prefix: "rl:refresh:",
 });
