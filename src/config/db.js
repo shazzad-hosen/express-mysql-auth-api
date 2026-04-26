160de79cfc48b305cead23dc97829d205939671b
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-04-26 14:49:18 +0600

    update env and db config to connect to online db

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,14 @@ import { ENV } from "./env.js";
 
 const pool = mysql.createPool({
   host: ENV.DB_HOST,
+  port: ENV.DB_PORT,
   user: ENV.DB_USER,
   password: ENV.DB_PASSWORD,
   database: ENV.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 
   waitForConnections: true,
   connectionLimit: 10,
--- src/config/env.js
+++ src/config/env.js
@@ -6,6 +6,7 @@ export const ENV = {
   PORT: process.env.PORT,
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
+  DB_PORT: process.env.DB_PORT,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_NAME: process.env.DB_NAME,
   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
