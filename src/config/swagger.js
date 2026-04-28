bae358f69ce297e2761fd834ca6aad96aa52dc2a
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-04-28 19:28:49 +0600

    update servers url for swagger ui

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000/api/auth",
+        url: "https://express-mysql-auth-api.onrender.com/api/auth",
       },
     ],
     tags: [
