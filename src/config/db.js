52616b390efe8757c3eccf462fe3580386961590
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-06-12 19:07:43 +0600

    debugging database

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,18 @@
 import mysql from "mysql2/promise";
 import { ENV } from "./env.js";
 
-const pool = mysql.createPool(ENV.DB_URL);
+console.log(`ENV: ${ENV}`);
+
+const pool = mysql.createPool({
+  host: ENV.DB_HOST,
+  port: ENV.DB_PORT,
+  user: ENV.DB_USER,
+  password: ENV.DB_PASSWORD,
+  database: ENV.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default pool;
