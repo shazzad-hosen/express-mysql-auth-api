64de9e6c7fa83918c3afc7a85bdcc5eeddb4955e
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-06-14 21:51:40 +0600

    testing tcp connection again

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,28 @@ import mysql from "mysql2/promise";
 import { ENV } from "./env.js";
 import fs from "fs";
 
-console.log("HOST:", ENV.DB_HOST);
+import net from "net";
+
+const socket = net.createConnection({
+  host: ENV.DB_HOST,
+  port: Number(ENV.DB_PORT),
+});
+
+socket.on("connect", () => {
+  console.log("TCP CONNECTED");
+  socket.destroy();
+});
+
+socket.on("error", (err) => {
+  console.error("TCP ERROR:", err);
+});
+
+socket.on("timeout", () => {
+  console.error("TCP TIMEOUT");
+  socket.destroy();
+});
+
+socket.setTimeout(10000);
 
 const pool = mysql.createPool({
   host: ENV.DB_HOST,
