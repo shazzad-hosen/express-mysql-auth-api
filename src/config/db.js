483ebc6e57ce244808043c35f734b335ef3b9d4b
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-06-15 21:49:31 +0600

    change db config

--- src/config/db.js
+++ src/config/db.js
@@ -2,32 +2,9 @@ import mysql from "mysql2/promise";
 import { ENV } from "./env.js";
 import fs from "fs";
 
-import net from "net";
-
-const socket = net.createConnection({
-  host: ENV.DB_HOST,
-  port: Number(ENV.DB_PORT),
-});
-
-socket.on("connect", () => {
-  console.log("TCP CONNECTED");
-  socket.destroy();
-});
-
-socket.on("error", (err) => {
-  console.error("TCP ERROR:", err);
-});
-
-socket.on("timeout", () => {
-  console.error("TCP TIMEOUT");
-  socket.destroy();
-});
-
-socket.setTimeout(10000);
-
 const pool = mysql.createPool({
   host: ENV.DB_HOST,
-  port: Number(ENV.DB_PORT),
+  port: parseInt(ENV.DB_PORT),
   user: ENV.DB_USER,
   password: ENV.DB_PASSWORD,
   database: ENV.DB_NAME,
@@ -35,7 +12,12 @@ const pool = mysql.createPool({
   ssl: {
     ca: fs.readFileSync("./certs/ca.pem"),
   },
-  connectTimeout: 10000,
+  connectTimeout: 20000,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 30000
 });
 
 export default pool;
