4f346288f5026a76250207e48fa4b503b1c3e920
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-06-19 15:12:01 +0600

    update configuration to previous one

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,17 @@
 import mysql from "mysql2/promise";
 import { ENV } from "./env.js";
-import fs from "fs";
 
 const pool = mysql.createPool({
   host: ENV.DB_HOST,
-  port: parseInt(ENV.DB_PORT),
+  port: ENV.DB_PORT,
   user: ENV.DB_USER,
   password: ENV.DB_PASSWORD,
   database: ENV.DB_NAME,
 
   ssl: {
-    ca: fs.readFileSync("./certs/ca.pem"),
     rejectUnauthorized: false,
   },
-  connectTimeout: 30000,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 10000,
+
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
